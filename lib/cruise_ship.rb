e4ccc9937a539ca7fcb23e0960dae1c82a335927
Mike Cooper <mikescooper@icloud.com>
2019-08-22 01:13:24 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,11 +8,11 @@
 
 def select_winner(passengers)
   winner = ""
-  
-  passengers.each { |suite, name|
+passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
     winner = name
-  end }
-
-winner 
+  end
+end
+ 
+winner
 end
